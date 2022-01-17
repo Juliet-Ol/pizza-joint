8e59f4cba66969283b13a547fa376c89990ca6ae
Juliet-Ol <agutjuliet@gmail.com>
2022-01-17 08:42:12 +0300

    add js function

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+
+function Cost (size, crust, toppings, flavour, quantity, delivery){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.flavour = flavour;
+    this.quantity = quantity;
+    this.delivery = delivery
+        
+  
+
+}
+
+
+// getting pizza total cost
+
+    var size = document.getElementById
+    var crust = document.getElementById
+    var toppings = document.getElementById
+    var flavour = document.getElementById
+    var quantity = document.getElementById
+    var delivery = document.getElementById
+    
+
+    // pizza size
